5c8e1e86af86a0cef28ce8d91abee55d21842113
tnicacio <nicacio.t92@gmail.com>
2021-03-10 21:01:45 -0300

    Adding leaderboard

--- src/components/leaderboard/LeaderboardRow.tsx
+++ src/components/leaderboard/LeaderboardRow.tsx
@@ -1,5 +1,18 @@
 import styles from '../../styles/components/LeaderboardRow.module.css';
 
-export function LeaderboardRow() {
-  return <div></div>;
+export function LeaderboardRow({ position, user }) {
+  const { name, imgUri, level, challengesCompleted, currentExperience } = user;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.userInfo}>
+        <div className={styles.position}>{position}</div>
+        <div className={styles.profile}>Perfil</div>
+      </div>
+      <div className={styles.challengeInfo}>
+        <div className={styles.challenges}>{challengesCompleted}</div>
+        <div className={styles.experience}>{currentExperience}</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -1,9 +1,7 @@
 import styles from '../../styles/pages/Leaderboard.module.css';
 import axios from 'axios';
-import Head from 'next/head';
-import { MenuBar } from '../../components/MenuBar';
 import { GeneralLayout } from '../../components/GeneralLayout';
-import { Card } from '../../components/about/Card';
+import { LeaderboardRow } from '../../components/leaderboard/LeaderboardRow';
 
 interface User {
   name: string;
@@ -20,8 +18,34 @@ interface LeaderboardProps {
 
 export default function Leaderboard({ userList }: LeaderboardProps) {
   console.log(userList);
-  return <GeneralLayout pageTitle="Leaderboard"></GeneralLayout>;
+  return (
+    <GeneralLayout pageTitle="Leaderboard">{showUsers(userList)}</GeneralLayout>
+  );
 }
+
+const showUsers = (userList) => {
+  return (
+    <div className={styles.containerExterno}>
+      <header className={styles.leaderboardHeader}>
+        <div className={styles.userInfoTitle}>
+          <div className={styles.positionTitle}>POSIÇÃO</div>
+          <div className={styles.userTitle}>USUÁRIO</div>
+        </div>
+        <div className={styles.challengeInfoTitle}>
+          <div className={styles.challengesTitle}>DESAFIOS</div>
+          <div className={styles.experienceTitle}>EXPERIÊNCIA</div>
+        </div>
+      </header>
+      <section className={styles.contentContainer}>
+        {userList?.length > 0
+          ? userList.map((user, index) => {
+              return <LeaderboardRow position={index + 1} user={user} />;
+            })
+          : 'Nada a mostrar'}
+      </section>
+    </div>
+  );
+};
 
 export async function getServerSideProps(ctx) {
   const getUsers = async () => {
